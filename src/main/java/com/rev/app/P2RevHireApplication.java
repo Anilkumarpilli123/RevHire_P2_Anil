dde6036c633043e04ca45d345b7716e9e8931464
Anilkumarpilli123 <anilkumarpilli333@gmail.com>
2026-02-27 14:41:31 +0530

    Initial commit - RevHire project setup

---
+++ src/main/java/com/rev/app/P2RevHireApplication.java
@@ -0,0 +1,13 @@
+package com.rev.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2RevHireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2RevHireApplication.class, args);
+    }
+
+}
