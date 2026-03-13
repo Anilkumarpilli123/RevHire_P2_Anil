05a938ce53225ad542c0640c5b2389dd578692fa
Anil Kumar <anilkumarpilli333@gmail.com>
2026-03-13 11:53:14 +0530

    updated changes

--- src/test/java/com/rev/app/RevHireP2ApplicationTests.java
+++ src/test/java/com/rev/app/RevHireP2ApplicationTests.java
@@ -2,9 +2,14 @@ package com.rev.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootTest
 class RevHireP2ApplicationTests {
+
+    @MockitoBean
+    private JavaMailSender javaMailSender;
 
     @Test
     void contextLoads() {
